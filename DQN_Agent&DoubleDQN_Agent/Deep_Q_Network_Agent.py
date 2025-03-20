a2305f10e5a80fce13c52af1f8a3964d839dd961
Malak Zaidi <malakzaidi815@gmail.com>
2025-03-20 11:29:57 +0000

    define the needed parameters

--- DQN_Agent&DoubleDQN_Agent/Deep_Q_Network_Agent.py
+++ DQN_Agent&DoubleDQN_Agent/Deep_Q_Network_Agent.py
@@ -6,3 +6,14 @@ from tensorflow.keras.layers import Dense
 from tensorflow.keras.optimizers import Adam
 from collections import deque
 
+GRID_SIZE = 4
+STATE_SIZE = GRID_SIZE * GRID_SIZE
+ACTION_SIZE = 4
+GAMMA = 0.9
+LEARNING_RATE = 0.01
+EPSILON = 1.0
+EPSILON_MIN = 0.01
+EPSILON_DECAY = 0.995
+BATCH_SIZE = 32
+MEMORY_SIZE = 2000
+EPISODES = 1000
