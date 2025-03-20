bb894ca806433a1fb7cb843395247d8a70f9650c
Malak Zaidi <malakzaidi815@gmail.com>
2025-03-20 11:28:36 +0000

    import the required libraries to implement the DQN Agent

---
+++ DQN_Agent&DoubleDQN_Agent/Deep_Q_Network_Agent.py
@@ -0,0 +1,8 @@
+import numpy as np
+import random
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.optimizers import Adam
+from collections import deque
+
